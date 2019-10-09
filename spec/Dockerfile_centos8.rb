9940239d2d1ce2da11ebfa34c295a963592eb69e
apolloclark <apolloclark@gmail.com>
2019-10-09 18:16:39 -0400

    upgraded to 12.10.0

---
+++ spec/Dockerfile_centos8.rb
@@ -0,0 +1,29 @@
+# spec/Dockerfile_spec.rb
+
+require_relative "spec_helper"
+
+describe "Dockerfile" do
+  before(:all) do
+    load_docker_image()
+    set :os, family: :redhat
+  end
+
+  describe "Dockerfile#running" do
+    it "runs the right version of CentOS" do
+      expect(os_version).to include("CentOS")
+      expect(os_version).to include("release 8")
+    end
+    it "runs as root" do
+      expect(sys_user).to eql("root")
+    end
+  end
+
+  describe package(ENV['PACKAGE_NAME']) do
+    it { should be_installed.with_version(ENV['PACKAGE_VERSION']) }
+  end
+
+  describe command("node --version") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should contain ENV['PACKAGE_VERSION'] }
+  end
+end
